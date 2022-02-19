48eb6614f331921fac9c3eeed9e4f119158c9c88
oksunwoo <oksunwo@naver.com>
2022-02-20 05:24:44 +0900

    feat: InsertionSort, BubbleSort, SelectionSort Sample

--- Toni/AlgorithmSample/AlgorithmSample/SelectionSort.swift
+++ Toni/AlgorithmSample/AlgorithmSample/SelectionSort.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+func selectionSort<Element: Comparable>(_ array: inout [Element]) {
+    guard array.count >= 2 else {
+      return
+    }
+    
+    for current in 0..<(array.count - 1) {
+        var lowest = current
+        
+        for other in (current + 1)..<array.count {
+            if array[lowest] > array[other] {
+                lowest = other
+            }
+        }
+        
+        if lowest != current {
+            array.swapAt(lowest, current)
+        }
+    }
+}
