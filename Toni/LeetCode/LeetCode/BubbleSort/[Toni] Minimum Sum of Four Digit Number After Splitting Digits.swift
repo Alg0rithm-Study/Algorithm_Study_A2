34c5c8d7f35e493324d5057b6f4adcb4c6f987d4
oksunwoo <oksunwo@naver.com>
2022-02-22 20:42:09 +0900

    chore: 프로그램을 설명하는 주석 추가

--- Toni/LeetCode/LeetCode/BubbleSort/[Toni] Minimum Sum of Four Digit Number After Splitting Digits.swift
+++ Toni/LeetCode/LeetCode/BubbleSort/[Toni] Minimum Sum of Four Digit Number After Splitting Digits.swift
@@ -5,7 +5,7 @@
 //  Created by Sunwoo on 2022/02/21.
 //
 /*
- 각각의 숫자로 만들수있는 가장 작은 수 두개를 만들어서 결과를 리턴하는 문제.
+ 각각의 숫자로 만들수있는 가장 작은 수 두개를 만들어서 결과를 리턴하는 문제. 가장 작은 숫자 2개가 10의 자리를 차지하면 될것이라고 생각.
  */
 
 import Foundation
@@ -14,12 +14,12 @@ class Solution21 {
     func minimumSum(_ num: Int) -> Int {
         var number = num
         
-        var arr = makeRemainder(of: &number)
+        var arr = makeRemainder(of: &number) //숫자를 잘게 쪼개기 위해 10으로 나워서 나머지를 모두 return
 
-        bubbleSort(&arr)
+        bubbleSort(&arr) // bubble sort를 이용하여 정렬 후
         
-        let a = arr[0] * 10 + arr[2] * 1
-        let b = arr[1] * 10 + arr[3] * 1
+        let a = arr[0] * 10 + arr[2] * 1 // 가장 작은 두수는 10의자리, 그다음수는 1의자리로
+        let b = arr[1] * 10 + arr[3] * 1 // 계산을 한뒤에 return
  
         return a + b
     }
