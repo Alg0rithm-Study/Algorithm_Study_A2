2b8dcc124014c55c48a77f815928eda18f6da442
oksunwoo <oksunwo@naver.com>
2022-04-13 01:42:27 +0900

    ds: BinaryTree

--- Toni/DataStructureSample/DataStructureSample/BinaryTree.swift
+++ Toni/DataStructureSample/DataStructureSample/BinaryTree.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+class BinaryNode<Element> {
+    var value: Element
+    var leftChild: BinaryNode?
+    var rightChild: BinaryNode?
+    
+    init(value: Element) {
+        self.value = value
+    }
+}
+
+extension BinaryNode {
+    func traverseInOrder(visit: (Element) -> Void) {
+        leftChild?.traverseInOrder(visit: visit)
+        visit(value)
+        rightChild?.traverseInOrder(visit: visit)
+    }
+    
+    func traversePreOrder(visit: (Element) -> Void) {
+        visit(value)
+        leftChild?.traversePreOrder(visit: visit)
+        rightChild?.traversePreOrder(visit: visit)
+    }
+    
+    func traversePostOrder(visit: (Element) -> Void) {
+        leftChild?.traversePostOrder(visit: visit)
+        rightChild?.traversePostOrder(visit: visit)
+        visit(value)
+    }
+}
---
+++ Toni/LeetCode/LeetCode/Graph/[Toni] Find if Path Exists in Graph.swift
@@ -0,0 +1,8 @@
+//
+//  [Toni] Find if Path Exists in Graph.swift
+//  LeetCode
+//
+//  Created by Sunwoo on 2022/04/13.
+//
+
+import Foundation
