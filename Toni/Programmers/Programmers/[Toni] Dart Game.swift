8f1209d921cc2f63e5a6cf4e25be4b18f61fb743
oksunwoo <oksunwo@naver.com>
2022-04-18 20:57:41 +0900

    programmers: 다트문제 주석 추가

--- Toni/Programmers/Programmers/[Toni] Dart Game.swift
+++ Toni/Programmers/Programmers/[Toni] Dart Game.swift
@@ -9,34 +9,33 @@ import Foundation
 
 func solution(_ dartResult:String) -> Int {
     var dart = dartResult
-    let score = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    let option = ["S", "D", "T"]
-    //let sign = ["*", "#"]
-    var result: [Int] = []
+    let score = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]                          // 해당 숫자가 들어오는지 판단하기 위한 배열
+    let bonus = ["S", "D", "T"]                                                            // 해당하는 보너스가 들어오는지 판별하기 위함. 이외의 것은 *,#이므로 else문으로 퉁침
+    var result: [Int] = []                                                                  // 결과는 앞으로 result 배열에 순서대로 쌓일 것.
     
-    while dart.count > 0 {
-        let first = String(dart.removeFirst())
+    while dart.count > 0 {                                                                  // 입력받은 문자열이 없어질때까지 루프
+        let first = String(dart.removeFirst())                                              // 첫번째 글자를 따온다
         
-        if score.contains(first) {
-            if first == "1" {
-                if score.contains(String(dart.first!)) {
-                    dart.removeFirst()
-                    result.append(getScore(num: "10"))
+        if score.contains(first) {                                                          // 그 글자가 score배열안에 있으면
+            if first == "1" {                                                               // 점수가 10점인지, 1점인지 판별해야되기 때문에.. 앞글자가 1이면
+                if score.contains(String(dart.first!)) {                                    // 다시 맨 앞이 숫자인지 확인. 그냥 0인지 확인해도 되겠군..
+                    dart.removeFirst()                                                      // 맞다면 0도 제거
+                    result.append(getScore(num: "10"))                                      // 스코어 얻는 곳에 10점을 넣어준다.
                 } else {
-                    result.append(getScore(num: first))
+                    result.append(getScore(num: first))                                     // 근데 다음 글자가 0이 아니면 1을 넣어주는것.
                 }
-            } else {
-                result.append(getScore(num: first))
+            } else {                                                                        // 맨앞이 1이 아닐경우
+                result.append(getScore(num: first))                                         // 해당 숫자 넣어줌
             }
-        } else if option.contains(first) {
-            let last = result.removeLast()
-            result.append(getBonus(bonus: first, score: last))
+        } else if bonus.contains(first) {                                                  // 만약 score에 해당하는 게 아니고, bonus에 해당하면
+            let last = result.removeLast()                                                  // 점수를 두배 또는 세배 해야되니까 result배열에서 마지막꺼 가져옴
+            result.append(getBonus(bonus: first, score: last))                              // 결과를 result에 append
         } else {
-            result = getOption(option: first, scoreArr: result)
+            result = getOption(option: first, scoreArr: result)                             // score도 아니고, bonus도 아니면 option
         }
     }
     
-    let answer = result.reduce(into: 0) { sum, n in
+    let answer = result.reduce(into: 0) { sum, n in                                         // 배열의 있는 값을 합산
         sum += n
     }
     
@@ -44,7 +43,7 @@ func solution(_ dartResult:String) -> Int {
 }
 
 func getScore(num: String) -> Int {
-    let answer = Int(num)!
+    let answer = Int(num)!                                                                  //숫자는 Int
     
     return answer
 }
@@ -52,10 +51,10 @@ func getScore(num: String) -> Int {
 func getBonus(bonus: String, score: Int) -> Int {
     var answer = score
     
-    if bonus == "D" {
-        answer = answer * answer
-    } else if bonus == "T" {
-        answer = answer * answer * answer
+    if bonus == "D" {                                                                       //D가 들어오면
+        answer = answer * answer                                                            // 제곱
+    } else if bonus == "T" {                                                                //T가 들어오면
+        answer = answer * answer * answer                                                   // 세제곱
     }
     
     return answer
@@ -63,15 +62,15 @@ func getBonus(bonus: String, score: Int) -> Int {
 
 func getOption(option: String, scoreArr: [Int]) -> [Int] {
     var scores = scoreArr
-    if scores.count == 1 {
+    if scores.count == 1 {                                                                  // 계산된 점수가 한개일경우
         if option == "*" {
-            scores[0] = scores[0] * 2
+            scores[0] = scores[0] * 2                                                       // 해당 것만 더블
         } else if option == "#" {
-            scores[0] = scores[0] * -1
+            scores[0] = scores[0] * -1                                                      // 또는 -
         } else {
-            
+                                                                                        // 빈 else는 위에서 그냥 else처리해버리면 *이 아닌경우에 모두 음수처리할것같았는데 없애도 되겠네
         }
-    } else {
+    } else {                                                                                //점수가 두개 이상일 경우~~
         let lastIndex = scores.count - 1
         if option == "*" {
             scores[lastIndex-1] = scores[lastIndex-1] * 2
