e985068fd20e3bf8ce5a0a14afb7264b04319aac
oksunwoo <oksunwo@naver.com>
2022-06-15 02:11:25 +0900

    chore: 주석추가

--- Toni/Programmers/Programmers/[Toni] Friends4Block.swift
+++ Toni/Programmers/Programmers/[Toni] Friends4Block.swift
@@ -9,41 +9,41 @@ import Foundation
 func solution16(_ m:Int, _ n:Int, _ board:[String]) -> Int {
     var line = [Character]()
     var game = [[Character]]()
-    var indexArr = [[Int]]()
-    var tempArr = [Int]()
+    var indexArr = [[Int]]()                    // 좌표를 보관할 용도
+    var tempArr = [Int]()                       // 좌표 보관용도
     var answer = 0
-    var flag = true
+    var flag = true                             // flag = true 될때까지 while문을 돌릴예정
     
-    for str in board {
+    for str in board {                          // board에 있는 str을
         for i in str {
-            line.append(i)
+            line.append(i)                      // 2중 배열로 바꿔줌
         }
         game.append(line)
         line.removeAll()
     }
     
-    while flag {
+    while flag {                                                    // flag가 true면
         flag = false
         
-        for i in 0..<m-1 {
+        for i in 0..<m-1 {                                          // 각 원소갯수만큼 루프를 돌건데
             for j in 0..<n-1 {
-                let firstCharacter = game[i][j]
-                let secondCharacter = game[i][j+1]
+                let firstCharacter = game[i][j]                     // 첫번째 캐릭터와
+                let secondCharacter = game[i][j+1]                  // 그 옆 두번째 캐릭터
 
-                if firstCharacter == "0" {
-                    continue
+                if firstCharacter == "0" {                          // 첫번째꺼가 0이면 (지워지는 애들을 0으로 바꿔줄거임)
+                    continue                                        // 밑에 검사 안함
                 }
                 
-                if firstCharacter == secondCharacter {
+                if firstCharacter == secondCharacter {              // 두개가 같으면
                     let thirdCharacter = game[i+1][j]
                     let fourthCharacter = game[i+1][j+1]
                     
-                    if thirdCharacter == fourthCharacter {
-                        if firstCharacter == thirdCharacter {
-                            flag = true
+                    if thirdCharacter == fourthCharacter {          // 그 아래 두개를 검사
+                        if firstCharacter == thirdCharacter {       // 두개는 서로 같으니, 위아래도 검사
+                            flag = true                             // 트루로 바꾸고
                             tempArr.append(i)
                             tempArr.append(j)
-                            indexArr.append(tempArr)
+                            indexArr.append(tempArr)                // 좌표를 넣는다.
                             tempArr.removeAll()
                             
                             tempArr.append(i)
@@ -66,36 +66,35 @@ func solution16(_ m:Int, _ n:Int, _ board:[String]) -> Int {
             }
         }
         
-        indexArr = Array(Set(indexArr))
+        indexArr = Array(Set(indexArr))                             // 중복좌표를 제거하고
         
         for index in indexArr {
             let i = index[0]
             let j = index[1]
             
-            game[i][j] = "0"
-            answer += 1
+            game[i][j] = "0"                                        // 터진애들을 0 처리
+            answer += 1                                             // 그리고 개수를 센다
         }
         
-        indexArr.removeAll()
+        indexArr.removeAll()                                        // 좌표부분을 비우고
         
-        var zeroFlag = true
+        var zeroFlag = true                                         // 0으로 만든부분을 맨위로 올려줄거임
         while zeroFlag {
             zeroFlag = false
             for i in 0..<m-1 {
                 for j in 0..<n {
-                    let first = game[i][j]
-                    let second = game[i+1][j]
+                    let first = game[i][j]                          // 위에있는애와
+                    let second = game[i+1][j]                       // 아래있는애를 비교할거임
                     
-                    if first != "0" && second == "0" {
+                    if first != "0" && second == "0" {              // 위에 인형이 있고 아래에는 공백이라면
                         zeroFlag = true
-                        game[i+1][j] = game[i][j]
+                        game[i+1][j] = game[i][j]                   // 인형은 가라앉는다
                         game[i][j] = "0"
-                    }
+                    }                                               // 이행위를 계속 반복
                 }
             }
         }
     }
-    
     return answer
 }
 
