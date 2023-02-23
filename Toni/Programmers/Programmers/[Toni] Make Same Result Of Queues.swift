98f3557822b8479843d4a2418f6888bfdaf5e50c
oksunwoo <oksunwo@naver.com>
2023-02-23 13:07:38 +0900

    programmers: 두큐합 같게 만들기

--- Toni/Programmers/Programmers/[Toni] Make Same Result Of Queues.swift
+++ Toni/Programmers/Programmers/[Toni] Make Same Result Of Queues.swift
@@ -7,29 +7,36 @@
 
 import Foundation
 
-func solution41(_ queue1:[Int], _ queue2:[Int]) -> Int {
-    let queue1sum = queue1.reduce(0, +)
-    let queue2sum = queue2.reduce(0, +)
-    let sum = (queue1sum + queue2sum) / 2
+func solution49(_ queue1:[Int], _ queue2:[Int]) -> Int {
+    let array = queue1 + queue2
+    var left = 0
+    var right = queue1.count
+    var count = 0
     
-    var position = 0
+    var queue1Sum = queue1.reduce(0, +)
+    let queue2Sum = queue2.reduce(0, +)
+    let goal = (queue1Sum + queue2Sum) / 2
     
-    for i in 0..<queue1.count {
-        if queue1[i] > sum {
-            return -1
-        }
-        
-        if queue2[i] > sum {
-            return -1
-        }
-        
-        if queue1[i] == sum || queue2[i] == sum {
-            position = i
-        }
+    if (queue1Sum + queue2Sum) % 2 != 0 {
+        return -1
     }
     
-    print(queue1sum)
-    print(queue2sum)
+    while right<array.count && left <= right {
+        if queue1Sum < goal {
+            queue1Sum += array[right]
+            right += 1
+        } else if queue1Sum > goal {
+            queue1Sum -= array[left]
+            left += 1
+        } else {
+            break
+        }
+        count += 1
+    }
     
-    return -2
+    if queue1Sum != goal {
+        return -1
+    }
+    
+    return count
 }
