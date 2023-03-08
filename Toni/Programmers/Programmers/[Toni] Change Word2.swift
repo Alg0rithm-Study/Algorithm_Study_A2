40aff64f9abeefc39caddef5d40cda03afb4149b
oksunwoo <oksunwo@naver.com>
2023-03-08 19:23:51 +0900

    programmers: 단어변환 다시

--- Toni/Programmers/Programmers/[Toni] Change Word2.swift
+++ Toni/Programmers/Programmers/[Toni] Change Word2.swift
@@ -8,67 +8,51 @@
 import Foundation
 
 func solution55(_ begin:String, _ target:String, _ words:[String]) -> Int {
-    var answer = 200000000000
-    var visited = Array(repeating: false, count: words.count)
-    
-    func dfs1(begin: String, target: String, count: Int) {
-        if begin == target {
-            if answer > count {
-                answer = count
-            }
-            return
-        } else {
-            
-   
-        
-        for i in 0..<words.count {
-            if visited[i] == true {
-                continue
-            }
-            
-            if checkOnlyOne(first: begin, second: words[i]) {
-                if words[i] == target {
-                    visited[i] = false
-                } else {
-                    visited[i] = true
+    var queue: [String] = []
+       queue.append(begin)
+       
+       var visited = [Bool] (repeating: false, count: words.count)
+       var count: Int = 0
+       
+       while !queue.isEmpty {
+           let now = queue.popLast()!
+           if now == target {
+               break
+           }
+           count += 1
+           
+            for i in 0..<words.count {
+                if !visited[i] {
+                    if checkOnlyOne(word: words[i], now: now) {
+                        queue.append(words[i])
+                        visited[i] = true
+                    }
                 }
-                
-                
-                dfs1(begin: words[i], target: target, count: count + 1)
             }
         }
-        }
-    }
-    
-    dfs1(begin: begin, target: target, count: 0)
-    
-    
-    print(answer)
-    
+    print(count)
     return 0
 }
 
-func checkOnlyOne(first: String, second: String) -> Bool {
-    var firstString = first
-    var secondString = second
-    var count = 0
-    
-    while firstString.isEmpty {
-        let ch1 = firstString.popLast()!
-        let ch2 = secondString.popLast()!
-        
-        if ch1 == ch2 {
-            count += 1
-        }
-        
-        if count == 2 {
-            break
-        }
-    }
-    
-    if count == 1 {
-        return true
-    }
-    
-    return false
+func checkOnlyOne(word: String, now: String) -> Bool {
+    if word.count != now.count {
+           return false
+       }
+       
+       var count: Int = 0
+       
+       let arrWord = Array(word)
+       let arrNow = Array(now)
+       
+       for i in 0..<arrWord.count {
+           if arrWord[i] != arrNow[i] {
+               count += 1
+           }
+           
+           if count > 1 {
+               return false
+           }
+       }
+       
+       return true
 }
